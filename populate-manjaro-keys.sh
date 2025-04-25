c6967ee75e79e13faf1793f8b406a00cf6e11efa
matheusmichalski <matheus.michalskifc@gmail.com>
2025-04-25 14:44:45 -0300

    RAW: enhance populate-manjaro-keys script with additional key refresh command

--- populate-manjaro-keys.sh
+++ populate-manjaro-keys.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 sudo rm -r /var/lib/pacman/sync
-sudo pacman -Sy
+sudo pacman -Syy
+sudo pacman -Syyu
 
 sudo pacman-key --init
 sudo pacman-key --populate manjaro
+
+sudo pacman-key --refresh-keys
 
 sudo pacman -Syu
