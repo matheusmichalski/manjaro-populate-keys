7724d80d409a47d77d9a058d27927cae10808f6f
matheusmichalski <matheus.michalskifc@gmail.com>
2025-04-25 15:28:02 -0300

    FIX: streamline populate-manjaro-keys script by removing unnecessary comments and commands

--- populate-manjaro-keys.sh
+++ populate-manjaro-keys.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Remove sync folder (be cautious, this can cause issues if not done for a specific reason)
 sudo rm -r /var/lib/pacman/sync
+sudo pacman-mirrors --fasttrack 
 
-# Refresh package database
 sudo pacman -Syy
-
-# Initialize pacman keyring
 sudo pacman-key --init
-
-# Populate keyring with Arch and Manjaro keys
 sudo pacman-key --populate archlinux
 sudo pacman-key --populate manjaro
 
-# Refresh the keys and allow weak key signatures
-sudo pacman-key --refresh-keys --allow-weak-key-signatures
+sudo pacman-key --refresh-keys
 
-# Update the system and upgrade packages
+sudo pacman -Syyu
 sudo pacman -Syu
